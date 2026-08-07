e3ee4ff7488f21d07d7e4f47247cb98eec787c85
Filipe Pinato Aguiar <fpinatoaguiar@gmail.com.br>
2026-08-07 19:56:33 -0300

    Normalize quotes and clean up team template

--- server.rb
+++ server.rb
@@ -1,7 +1,7 @@
-require 'sinatra/base'
-require 'sinatra/reloader'
-require 'pry'
-require_relative 'lib/poke_api'
+require "sinatra/base"
+require "sinatra/reloader"
+require "pry"
+require_relative "lib/poke_api"
 
 class Server < Sinatra::Base
   configure :development do
@@ -10,30 +10,30 @@ class Server < Sinatra::Base
 
   configure do
     enable :logging
-    set :bind, '0.0.0.0'
+    set :bind, "0.0.0.0"
     set :port, 3000
     set :views, "views"
     set :team, []
     register Sinatra::Reloader
   end
 
-  get '/' do
+  get "/" do
     @pokemons = PokeApi.all
     erb :index
   end
 
-  get '/pokemon' do
+  get "/pokemon" do
     @pokemon = PokeApi.find(params[:name])
     erb :pokemon
   end
 
-  post '/team' do
+  post "/team" do
     @team = settings.team
     @team << PokeApi.find(params[:pokeName])
     erb :team
   end
 
-  get '/team' do
+  get "/team" do
     @team = settings.team
     @team.delete_at(params[:index].to_i)
     erb :team
