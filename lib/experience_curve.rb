77952c5147db0b6bd28362b92fb58f612aec01dd
Filipe Pinato Aguiar <fpinatoaguiar@gmail.com.br>
2026-08-10 18:56:01 -0300

    Passo 2: ExperienceCurve puro (curva linear xp_needed = level x 100, level_for_xp via acumulado)

---
+++ lib/experience_curve.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class ExperienceCurve
+  class << self
+    def xp_needed(level)
+      level * 100
+    end
+
+    def level_for_xp(total_xp)
+      level = 1
+      level += 1 while total_xp >= cumulative_xp_for(level)
+      level
+    end
+
+    private
+
+    def cumulative_xp_for(level)
+      level * (level + 1) * 100 / 2
+    end
+  end
+end
---
+++ test/experience_curve_test.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "minitest/autorun"
+require_relative "../lib/experience_curve"
+
+class ExperienceCurveTest < Minitest::Test
+  def test_xp_needed_is_linear_level_times_hundred
+    assert_equal 100, ExperienceCurve.xp_needed(1)
+    assert_equal 200, ExperienceCurve.xp_needed(2)
+    assert_equal 500, ExperienceCurve.xp_needed(5)
+  end
+
+  def test_level_for_xp_maps_cumulative_ranges
+    assert_equal 1, ExperienceCurve.level_for_xp(0)
+    assert_equal 1, ExperienceCurve.level_for_xp(99)
+    assert_equal 2, ExperienceCurve.level_for_xp(100)
+    assert_equal 2, ExperienceCurve.level_for_xp(299)
+    assert_equal 3, ExperienceCurve.level_for_xp(300)
+    assert_equal 3, ExperienceCurve.level_for_xp(599)
+    assert_equal 4, ExperienceCurve.level_for_xp(600)
+    assert_equal 4, ExperienceCurve.level_for_xp(999)
+    assert_equal 5, ExperienceCurve.level_for_xp(1000)
+  end
+
+  def test_level_for_xp_never_below_one
+    assert_equal 1, ExperienceCurve.level_for_xp(-5)
+    assert_equal 1, ExperienceCurve.level_for_xp(0)
+  end
+end
